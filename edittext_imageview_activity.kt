820f67f7c765ccfc3d0a5db97da58084d9c257e0
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-10-12 15:10:00 +0530

    Add files via upload
    A basic signup page (uset interface)

---
+++ edittext_imageview_activity.kt
@@ -0,0 +1,12 @@
+package com.example.Layoutsection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sampleapp.R
+
+class edittext_imageview_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edittext_imageview)
+    }
+}
