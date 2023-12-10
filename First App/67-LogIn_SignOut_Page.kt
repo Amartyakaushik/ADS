74bf87fdb5b1f12c2c60e15d18c07d81b04587fa
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-12-10 20:28:12 +0530

    here i have provided a logout button to logout from current logged in account

---
+++ First App/67-LogIn_SignOut_Page.kt
@@ -0,0 +1,59 @@
+package com.example.sampleapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sampleapp.databinding.ActivityLogInSignOutPageBinding
+import com.example.sampleapp.databinding.ActivitySignUpPageBinding
+import com.google.firebase.auth.FirebaseAuth
+//import com.firebase.ui.auth.AuthUI
+
+class LogIn_SignOut_Page : AppCompatActivity() {
+    private val binding: ActivityLogInSignOutPageBinding by lazy {
+        ActivityLogInSignOutPageBinding.inflate(layoutInflater)
+    }
+    private lateinit var auth:FirebaseAuth
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        // Initializing
+        auth=FirebaseAuth.getInstance()
+
+//        private fun signOut(){
+//            auth.getInstance()
+//                .signOut(this)
+//                .addOnCompleteListener {task ->
+//                    // Handle the sign-out completion if needed
+//                    if (task.isSuccessful) {
+//                        // After signing out, navigate to the login page or any other desired destination
+//                        val intent = Intent(this, LogIn_page::class.java)
+//                        startActivity(intent)
+//
+//                        // Finish the current activity to prevent the user from coming back to the logged-in page using the back button
+//                        finish()
+//                    } else {
+//                        // Handle sign-out failure if needed
+//                    }
+//                }
+//        }
+
+
+        fun signOut() {
+            auth.signOut()
+
+            // After signing out, navigate to the login page or any other desired destination
+            val intent = Intent(this, LogIn_page::class.java)
+            startActivity(intent)
+
+            // Finish the current activity to prevent the user from coming back to the logged-in page using the back button
+            finish()
+        }
+
+        binding.logOutButton.setOnClickListener {
+            signOut()
+        }
+    }
+
+}
