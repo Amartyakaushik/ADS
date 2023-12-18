950266b9c13df833453e290bfa4a4f33c50f7998
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-12-18 22:02:08 +0530

    Add files via upload

---
+++ GoogleAuthenticationActivity.kt
@@ -0,0 +1,75 @@
+package com.example.sampleapp
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.sampleapp.databinding.ActivityGoogleAuthenticationBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.auth
+
+class GoogleAuthenticationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGoogleAuthenticationBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    @SuppressLint("SuspiciousIndentation")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialization
+        binding = ActivityGoogleAuthenticationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+
+        binding.GoogleSignIn.setOnClickListener {
+            var signInClient = googleSignInClient.signInIntent
+            Launcher.launch(signInClient)
+        }
+    }
+
+    private val Launcher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode== Activity.RESULT_OK){
+            val task=GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            if(task.isSuccessful){
+                val account:GoogleSignInAccount?=task.result
+                val credential=GoogleAuthProvider.getCredential(account?.idToken,null)
+                auth.signInWithCredential(credential).addOnCompleteListener{
+                    if(it.isSuccessful){
+//                        Toast.makeText(this,"Done",Toast.LENGTH_LONG).show()
+                        startActivity(Intent(this,SignOut02Activity::class.java))
+                    }else{
+                        Toast.makeText(this,"Fail",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this,"Failed",Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+//        // to check is user is already logged in
+//        override fun onStart() {
+//            super.onStart()
+//            if(auth.currentUser!=null){
+//                startActivity(Intent(this,SignOut02Activity::class.java))
+//            }
+//        }
+}
+
+//translatable="false">YOUR_WEB_CLIENT_ID
