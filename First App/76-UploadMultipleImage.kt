c5c3ff0b15c7c5bce4b7fbe407b0c93d1b6d58f3
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2023-12-21 19:01:35 +0530

    A small change to grant the permission to upload multiple image saved in a particular folder.

---
+++ First App/76-UploadMultipleImage.kt
@@ -0,0 +1,97 @@
+package com.example.sampleapp
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.MimeTypeMap
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.sampleapp.databinding.ActivityPhotoUploadBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+import com.google.firebase.storage.storage
+import com.squareup.picasso.Picasso
+
+class PhotoUploadActivity : AppCompatActivity() {
+    lateinit var binding:ActivityPhotoUploadBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityPhotoUploadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.uploadImageButton.setOnClickListener {
+            val intent=Intent()
+            intent.action=Intent.ACTION_PICK
+            intent.type="image/*"
+            imageLauncher.launch(intent)
+
+        }
+    }
+
+    // Decalration of imageLaucher
+    val imageLauncher=registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode== Activity.RESULT_OK){
+            if (it.data!=null){
+//                lecture 76- Upload multiple images
+                val ref=Firebase.storage.reference.child("Photo/"+System.currentTimeMillis()+"."+getFileType(it.data!!.data!!))
+//                val ref=Firebase.storage.reference.child("photo")
+                ref.putFile(it.data!!.data!!).addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener {
+//                        lecture 76- Upload multiple images
+                        Firebase.database.reference.child("Photo").push().setValue(it.toString())
+//                        binding.myImageView.setImageURI(it)
+                        Toast.makeText(this@PhotoUploadActivity,"Photo Uploaded",Toast.LENGTH_LONG).show()
+                        Picasso.get().load(it.toString()).into(binding.myImageView);
+                    }
+                }
+            }
+        }
+    }
+//    lecture 76- Upload multiple images
+    private fun getFileType(data: Uri?): String? {
+        val mimeType=MimeTypeMap.getSingleton()
+        return mimeType.getMimeTypeFromExtension(contentResolver.getType(data!!))
+    }
+
+//    val imageLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+//        if (result.resultCode == Activity.RESULT_OK) {
+//            result.data?.data?.let { imageUri ->
+//                // Check if the selected data is an image
+//                if (imageUri.toString().startsWith("content://") || imageUri.toString()
+//                        .startsWith("file://")
+//                ) {
+//                    val ref = Firebase.storage.reference.child("photo")
+//                    ref.putFile(imageUri).addOnSuccessListener { taskSnapshot ->
+//                        ref.downloadUrl.addOnSuccessListener { downloadUri ->
+//                            // Use Picasso to load the image into ImageView
+//                            Picasso.get().load(downloadUri.toString()).into(binding.myImageView)
+//                            Toast.makeText(
+//                                this@PhotoUploadActivity,
+//                                "Photo Uploaded",
+//                                Toast.LENGTH_LONG
+//                            ).show()
+//                        }
+//                    }.addOnFailureListener {
+//                        Toast.makeText(
+//                            this@PhotoUploadActivity,
+//                            "Failed to upload photo",
+//                            Toast.LENGTH_LONG
+//                        ).show()
+//                    }
+//                } else {
+//                    Toast.makeText(
+//                        this@PhotoUploadActivity,
+//                        "Selected file is not an image",
+//                        Toast.LENGTH_LONG
+//                    ).show()
+//                }
+//            }
+//        }
+//
+//    }
+
+
+
+}
