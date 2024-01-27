2ba93243406a98af4fe7b1a93d25dd484e0983f5
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-01-28 02:21:07 +0530

    Add files via upload

---
+++ BottomNavBar.kt
@@ -0,0 +1,12 @@
+package com.example.lpuliveclone.BottomNavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lpuliveclone.R
+
+class BottomNavBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_nav_bar)
+    }
+}
