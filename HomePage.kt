385363957e6aff4926b16dd35268a50e704b0f62
Amartya kaushik <115655337+Amartyakaushik@users.noreply.github.com>
2024-01-28 22:04:02 +0530

    Add files via upload

--- HomePage.kt
+++ HomePage.kt
@@ -1,11 +1,43 @@
-package com.example.lpuliveclone
+package com.example.lpuliveclone.Fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.lpuliveclone.R
+import com.example.lpuliveclone.RvModel
+import com.example.lpuliveclone.adapter.RvAdapter
+import com.example.lpuliveclone.databinding.FragmentHomePageBinding
 
-class HomePage : AppCompatActivity() {
+class HomePage : Fragment() {
+    private lateinit var binding:FragmentHomePageBinding
+    private lateinit var rvAdapter: RvAdapter
+    private lateinit var dataList: ArrayList<RvModel>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding=FragmentHomePageBinding.inflate(layoutInflater)
     }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_page, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        dataList=ArrayList<RvModel>()
+        rvAdapter= RvAdapter(dataList, requireContext())
+        binding.rv.layoutManager=LinearLayoutManager(requireContext())
+//        binding.rv.layoutManager=GridLayoutManager(requireContext(),4)
+        binding.rv.adapter=rvAdapter
+
+    }
+
+//    companion object {
+//    }
 }
